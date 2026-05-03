a077be2adef807d7c6f2925243a4293a3bf443ab
Markku Rossi <mtr@iki.fi>
2026-05-03 08:41:34 +0200

    Refactoring kernel source files.

--- kernel/posix.go
+++ kernel/posix.go
@@ -4,14 +4,8 @@
 // All rights reserved.
 //
 
-// Package kerlen implements the emulator kernel.
+// Package kernel implements the emulator kernel.
 package kernel
-
-import (
-	"os"
-
-	"github.com/markkurossi/riscv/cpu"
-)
 
 type Kernel struct {
 	NextPID uint64
@@ -33,39 +27,4 @@ func (kern *Kernel) NewProcess(tg *Process) *Process {
 	}
 
 	return proc
-}
-
-type Process struct {
-	Kernel *Kernel
-	PID    uint64
-	TGID   uint64
-	CPU    *cpu.CPU
-	FDs    []*os.File
-	Ktrace bool
-}
-
-func (proc *Process) AllocFD(f *os.File) int {
-	for fd, file := range proc.FDs {
-		if file == nil {
-			proc.FDs[fd] = f
-			return fd
-		}
-	}
-	proc.FDs = append(proc.FDs, f)
-	return len(proc.FDs) - 1
-}
-
-func (proc *Process) CloseFD(fd int) bool {
-	if fd < 0 || fd >= len(proc.FDs) || proc.FDs[fd] == nil {
-		return false
-	}
-	proc.FDs[fd].Close()
-	return true
-}
-
-func (proc *Process) GetFD(fd int) *os.File {
-	if fd < 0 || fd >= len(proc.FDs) {
-		return nil
-	}
-	return proc.FDs[fd]
 }
